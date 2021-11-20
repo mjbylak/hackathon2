669353be5d2248e2e6fd5388877ce05a574cd258
TheLevic <levicrider08@gmail.com>
2021-11-20 11:47:22 -0600

    Sort of working

--- test.cpp
+++ test.cpp
@@ -8,39 +8,50 @@
 using namespace std;
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
-//   fprintf(stderr, "%s: ", (const char*)data);
-
-  /* for(i = 0; i<argc; i++){
-      printf("%s = %s\n", azColName[i],argv[i] ? argv[i] : "NULL");
-   }
-*/
 ofstream file;
 file.open("file.txt");
    for(i = 0; i<argc; i++){
-      file << /*"%s = %s\n", azColName[i],*/argv[i] ? argv[i] : "NULL";
+      file << argv[i] ? argv[i] : "NULL";
    }
    file.close();
-  // printf("\n");
    return 0;
 }
 
 int main(int argc, char* argv[]) {
+   //Declarations
+   bool run;
+   string allergies;
+   char *choice = new char[20];
+
+   //Getting data from user
+   cout << "What are your food allergies: ";
+   cin >> allergies;
+   cout << "What would you like to eat today? " << endl
+        << "1. Snickers" << endl
+        << "2. Doritos" << endl
+        << "3. Lucky Charms" << endl
+        << "4. Kitkat" << endl
+        << "5. Takis" << endl
+        << "6. Cheezit" << endl
+        << "7. Cheetos" << endl
+        << "8. Pringles" << endl
+        << "9. Peanut Butter" << endl
+        << "10. Oreos" << endl;
+
+   cin >> choice;
+
+
+
+
+   //Getting data from database
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
    char *sql;
    const char* data = "Callback function called";
-   char *statement = "Snickers";
-
+   char *statement = choice; //Need to change to input
    /* Open database */
    rc = sqlite3_open("Food.db", &db);
-   
-   if( rc ) {
- //     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
- //     fprintf(stderr, "Opened database successfully\n");
-   }
 
    /* Create SQL statement */
    sql = "SELECT * from food where Name = '";
@@ -58,5 +69,14 @@ int main(int argc, char* argv[]) {
 //      fprintf(stdout, "Operation done successfully\n");
    }
    sqlite3_close(db);
-   return 0;
+
+int allergen;
+   //Getting data from file
+   string fromFile;
+   ifstream file;
+   file.open("file.txt");
+   file.close();
+   cout << fromFile;
+return 0;
 }
+
